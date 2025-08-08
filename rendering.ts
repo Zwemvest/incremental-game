4a974256db263e7523ed92e9ad39b697379c6b3a
Magne Skjæran <github@meneth.com>
2025-08-08 10:37:34 +0200

    Sectioning

--- rendering.ts
+++ rendering.ts
@@ -2,7 +2,51 @@ import { Task, TaskDefinition, Skill } from "./zones.js";
 import { clickTask, SkillProgress, calcSkillXpNeeded } from "./simulation.js";
 import { GAMESTATE, RENDERING } from "./game.js";
 
+// MARK: Skills
+
 let SKILL_NAMES = ["Studying", "Travel"];
+
+function createSkillDiv(skill: SkillProgress, skills_div: HTMLElement, rendering: Rendering) {
+    const skill_div = document.createElement("div");
+    skill_div.className = "skill";
+
+    const name = document.createElement("div");
+    name.className = "skill-name";
+    name.textContent = `${SKILL_NAMES[skill.skill]}`;
+
+    const progressFill = document.createElement("div");
+    progressFill.className = "progress-fill";
+    progressFill.style.width = "0%";
+    const progressBar = document.createElement("div");
+    progressBar.className = "progress-bar";
+    progressBar.appendChild(progressFill);
+
+    skill_div.appendChild(name);
+    skill_div.appendChild(progressBar);
+
+    skills_div.appendChild(skill_div);
+    rendering.skill_elements.set(skill.skill, skill_div);
+}
+
+function updateSkillRendering() {
+    for (const skill of GAMESTATE.skills) {
+        var fill = RENDERING.skill_elements.get(skill.skill)?.querySelector<HTMLDivElement>(".progress-fill");
+        if (fill) {
+            fill.style.width = `${skill.progress * 100 / calcSkillXpNeeded(skill)}%`;
+        }
+
+        var name = RENDERING.skill_elements.get(skill.skill)?.querySelector<HTMLDivElement>(".skill-name");
+        if (name) {
+            const new_html = `${SKILL_NAMES[skill.skill]}<br>(${skill.level})`;
+            // Avoid flickering in the debugger
+            if (new_html != name.innerHTML) {
+                name.innerHTML = new_html;
+            }
+        }
+    }
+}
+
+// MARK: Tasks
 
 function createTaskDiv(task: Task, tasks_div: HTMLElement, rendering: Rendering) {
     const task_div = document.createElement("div");
@@ -27,27 +71,46 @@ function createTaskDiv(task: Task, tasks_div: HTMLElement, rendering: Rendering)
     rendering.task_elements.set(task.definition, task_div);
 }
 
-function createSkillDiv(skill: SkillProgress, skills_div: HTMLElement, rendering: Rendering) {
-    const skill_div = document.createElement("div");
-    skill_div.className = "skill";
+function updateTaskRendering() {
+    if (RENDERING.current_zone != GAMESTATE.current_zone)
+    {
+        RENDERING.current_zone = GAMESTATE.current_zone;
+        RENDERING.createTasks();
+    }
 
-    const name = document.createElement("div");
-    name.className = "skill-name";
-    name.textContent = `${SKILL_NAMES[skill.skill]}`;
+    // TODO - Track this with a reset count instead
+    if (GAMESTATE.did_energy_reset_this_tick) {
+        RENDERING.createTasks();
+    }
 
-    const progressFill = document.createElement("div");
-    progressFill.className = "progress-fill";
-    progressFill.style.width = "0%";
-    const progressBar = document.createElement("div");
-    progressBar.className = "progress-bar";
-    progressBar.appendChild(progressFill);
+    for (const task of GAMESTATE.tasks) {
+        var fill = RENDERING.task_elements.get(task.definition)?.querySelector<HTMLDivElement>(".progress-fill");
+        if (!fill) {
+            continue;
+        }
 
-    skill_div.appendChild(name);
-    skill_div.appendChild(progressBar);
-
-    skills_div.appendChild(skill_div);
-    rendering.skill_elements.set(skill.skill, skill_div);
+        fill.style.width = `${task.progress * 100 / task.definition.max_progress}%`;
+    }
 }
+
+// MARK: Energy
+function updateEnergyRendering() {
+    var fill = RENDERING.energy_element.querySelector<HTMLDivElement>(".progress-fill");
+    if (fill) {
+        fill.style.width = `${GAMESTATE.current_energy * 100 / GAMESTATE.max_energy}%`;
+    }
+
+    var value = RENDERING.energy_element.querySelector<HTMLDivElement>(".progress-value");
+    if (value) {
+        const new_html = `${GAMESTATE.current_energy}`;
+        // Avoid flickering in the debugger
+        if (new_html != value.innerHTML) {
+            value.textContent = new_html;
+        }
+    }
+}
+
+// MARK: Rendering
 
 export class Rendering {
     energy_element: HTMLElement;
@@ -92,62 +155,6 @@ export class Rendering {
         else {
             console.error("The element with ID 'energy' was not found.");
             this.energy_element = new HTMLElement();
-        }
-    }
-}
-
-function updateTaskRendering() {
-    if (RENDERING.current_zone != GAMESTATE.current_zone)
-    {
-        RENDERING.current_zone = GAMESTATE.current_zone;
-        RENDERING.createTasks();
-    }
-
-    // TODO - Track this with a reset count instead
-    if (GAMESTATE.did_energy_reset_this_tick) {
-        RENDERING.createTasks();
-    }
-
-    for (const task of GAMESTATE.tasks) {
-        var fill = RENDERING.task_elements.get(task.definition)?.querySelector<HTMLDivElement>(".progress-fill");
-        if (!fill) {
-            continue;
-        }
-
-        fill.style.width = `${task.progress * 100 / task.definition.max_progress}%`;
-    }
-}
-
-function updateSkillRendering() {
-    for (const skill of GAMESTATE.skills) {
-        var fill = RENDERING.skill_elements.get(skill.skill)?.querySelector<HTMLDivElement>(".progress-fill");
-        if (fill) {
-            fill.style.width = `${skill.progress * 100 / calcSkillXpNeeded(skill)}%`;
-        }
-
-        var name = RENDERING.skill_elements.get(skill.skill)?.querySelector<HTMLDivElement>(".skill-name");
-        if (name) {
-            const new_html = `${SKILL_NAMES[skill.skill]}<br>(${skill.level})`;
-            // Avoid flickering in the debugger
-            if (new_html != name.innerHTML) {
-                name.innerHTML = new_html;
-            }
-        }
-    }
-}
-
-function updateEnergyRendering() {
-    var fill = RENDERING.energy_element.querySelector<HTMLDivElement>(".progress-fill");
-    if (fill) {
-        fill.style.width = `${GAMESTATE.current_energy * 100 / GAMESTATE.max_energy}%`;
-    }
-
-    var value = RENDERING.energy_element.querySelector<HTMLDivElement>(".progress-value");
-    if (value) {
-        const new_html = `${GAMESTATE.current_energy}`;
-        // Avoid flickering in the debugger
-        if (new_html != value.innerHTML) {
-            value.textContent = new_html;
         }
     }
 }
